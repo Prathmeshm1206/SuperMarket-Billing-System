91884eb63926a05ec8e3209188d32634e3917ec1
Prathmeshm1206 <prathmeshm1206@gmail.com>
2023-11-16 19:38:50 +0530

    .

--- Billing1.cpp
+++ Billing1.cpp
@@ -8,7 +8,7 @@ class shopping
     private:
     int pcode;
     float price;
-    float discount;
+    float dis;
     string pname;
 
     public:
@@ -75,12 +75,13 @@ void shopping :: menu()
     {
         cout<<"please select from given options";
     }
-    }
+}
 goto m; 
 }
 
 void shopping :: administrator()
 {
+    m:
     int choice ;
     cout<<"\n\n\n\t\t\t Administrator menu";
     cout<<"\n\t\t\t|_____1) Add the Product_____|";
@@ -89,12 +90,101 @@ void shopping :: administrator()
     cout<<"\n\t\t\t|                            |";
     cout<<"\n\t\t\t|_____3) Delete the Product__|";
     cout<<"\n\t\t\t|                            |";
+    cout<<"\n\t\t\t|_____4) Back to main menu___|";
 
+    cout<<"\n\n\t Please Enter your choice";
 
+    cin>>choice;
+
+    switch (choice)
+    {
+    case 1:
+        add();
+        break;
+    case 2:
+        edit();
+        break;    
+    case 3:
+        rem();
+        break;
+    case 4:
+        menu();    
+    default:
+        cout<<"Invalid choice! ";
+     
+    }
+   goto m;
 
 }
 
+void shopping :: buyer()
+{
+    m:
+   int choice;
+   cout<<"\t\t\t Buyer \n";
+   cout<<"\t\t\t _________________\n";
+   cout<<"                \n";
+   cout<<"\t\t\t 1) Buy product \n";
+   cout<<"                \n";
+   cout<<"\t\t\t 2) Go back  \n";
+   cout<<"\t\t\t Enter your choice : ";
+   cin>>choice;
 
+   switch (choice)
+   {
+   case 1:
+       receipt();
+       break;
+   case 2:
+       menu();
+
+   default:
+     cout<<"Invalid Choice";
+    break;
+   }
+   goto m;
+}
+
+void shopping :: add()
+{
+    fstream data;                 
+    int c ;
+    int token=0;
+    float p;
+    float d;
+    string n;
+
+    cout<<"\n\n\t\t\t Add new product";
+    cout<<"\n\n\t Product code of the product";
+    cin>>pcode;
+    cout<<"\n\n Name of the product";
+    cin>>pname;
+    cout<<"\n\n\t Price of the product";
+    cin>>price;
+    cout<<"\n\n\t Discount on the product";
+    cin>>dis;
+
+    data.open("database.txt",ios::in);
+    if(!data)
+    {
+        data.open("database.txt",ios::app|ios::out);        //This ios prefix is used to open a file and append it to the end.
+        data<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<" "<<"\n";
+        data.close();
+    }
+    else{
+        data>>c>>n>>p>>d;
+        while (!data.eof())                  //End of file fuction
+                                            //while loop for checking every contant of file
+     {
+            if(c==pcode)
+            {
+                token++;
+            }
+            data>>c>>n>>p>>d;
+        }
+        
+    }
+}
 
 
 
